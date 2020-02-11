cbf26d605c507ad455e7a744cac3237004335fe6
Michael Stiles <mstiles01@icloud.com>
2020-02-11 16:12:06 -0600

    converted F to C

--- weatherapp.js
+++ weatherapp.js
@@ -4,7 +4,8 @@ window.addEventListener('load', () =>{
     let temperatureDescription = document.querySelector(".temperature-description");
     let temperatureDegree = document.querySelector(".temperature-degree");
     let locationTimezone = document.querySelector(".location-timezone");
-
+    let temperatureSection = document.querySelector(".degree-section");
+    const temperatureSpan = document.querySelector(".degree-section span")
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
@@ -26,8 +27,23 @@ window.addEventListener('load', () =>{
                 temperatureDegree.textContent = temperature;
                 temperatureDescription.textContent = summary;
                 locationTimezone.textContent = data.timezone;
+
+                //FORMULA FOR CELSIUS
+                let celsius = (temperature -32) * (5 / 9);
                 //Set Icon
                 setIcons(icon, document.querySelector('.icon'));
+
+                //Change temp to cELSIUS/F
+                temperatureSection.addEventListener('click', () =>{
+                    if(temperatureSpan.textContent === "F"){
+                        temperatureSpan.textContent= "C";
+                        temperatureDegree.textContent = Math.floor(celsius);
+                    } else {
+                        temperatureSpan.textContent = "F";
+                        temperatureDegree.textContent = temperature;
+                    }
+                }
+                )
             });
            
         });
