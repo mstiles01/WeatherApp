0a4351ebc7ed2b7bae633d32100a96266ecf5b39
Michael Stiles <mstiles01@icloud.com>
2020-02-11 15:35:23 -0600

    update temp and descp:

--- weatherapp.js
+++ weatherapp.js
@@ -1,6 +1,11 @@
 window.addEventListener('load', () =>{
     let long;
     let lat;
+    let temperatureDescription = document.querySelector(".temperature-description");
+    let temperatureDegree = document.querySelector(".temperature-degree");
+    let locationTimezone = document.querySelector(".location-timezone");
+
+
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
@@ -17,8 +22,10 @@ window.addEventListener('load', () =>{
             .then(data => {
                 console.log(data)
                 const {temperature, summary} = data.currently
-
-            })
+                temperatureDegree.textContent = temperature;
+                temperatureDescription.textContent = summary;
+            });
+           
         });
 
 
